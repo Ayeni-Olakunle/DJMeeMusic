3d0f244e67c56bdd58e43c800a4f649ccf47b545
Ayeni Emmanuel Olakunle <ayeniolakunle98@gmail.com>
2023-09-07 15:52:05 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Your API proxy configurations, if needed.
+    },
+    // Add this configuration for history API fallback
+    hmr: {
+      protocol: "ws",
+    },
+    // Add this configuration for history API fallback
+    cors: true,
+  },
+});
